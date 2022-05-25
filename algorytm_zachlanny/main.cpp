c1be7587da439f69f140e412b8e1b23c6c37b28b
Pomarancz789 <granat-02krawcy@icloud.com>
2022-05-25 10:23:54 +0200

    deez nuts

--- algorytm_zachlanny/main.cpp
+++ algorytm_zachlanny/main.cpp
@@ -1,14 +1,19 @@
-//
-//  main.cpp
-//  algorytm_zachlanny
-//
-//  Created by Jakub Wtorkiewicz on 25/05/2022.
-//
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    int nominaly[] = {500,200,100,50,20,10};
+    int ilebanknotow[] = {0,0,0,0,0,0};
+    int pensja = 1830;
+    
+    for (int i=0; i<6; i++) {
+        if(pensja>=nominaly[i])
+        {
+            ilebanknotow[i]=pensja/nominaly[i];
+            cout << nominaly[i] << ": " << ilebanknotow[i] << endl;
+            pensja=pensja%nominaly[i];
+        }
+    }
+    
     return 0;
 }
